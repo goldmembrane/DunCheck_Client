cb8c31d264379281c3559e6d5f4a668bb8360f1e
goldmembrane <byenghyunhwang@Byengui-MacBook-Pro.local>
2021-07-02 17:27:03 +0900

    create setting page and add setting button in App.tsx file

---
+++ src/page/Setting.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Setting = () => {
+    return (
+        <>
+            <div className = "settingWrapBox">
+                <div className = "settingCheckOptionBox">
+                    <ul className = "settingCheckOptionCollection">
+                        <li className = "settingCheckOption">마부</li>
+                        <li className = "settingCheckOption">딜표</li>
+                        <li className = "settingCheckOption">버프력</li>
+                    </ul>
+                </div>
+                <div className = "settingContentBox">
+                    
+                </div>
+            </div>
+        </>
+    )
+}
