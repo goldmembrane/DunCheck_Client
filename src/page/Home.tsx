180636598f823d050049f57052df75a634952f3a
goldmembrane <byenghyunhwang@Byengui-MacBook-Pro.local>
2021-07-06 21:54:47 +0900

    delete route/index.tsx file and create sub-routing for setting.tsx file

--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { RouteComponentProps } from 'react-router';
+import { RouteComponentProps } from 'react-router-dom';
 
 const Home = (props: RouteComponentProps) => {
 
--- src/page/Setting.tsx
+++ src/page/Setting.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
-import { RouteComponentProps } from 'react-router';
+import { RouteComponentProps, Link, Route } from 'react-router-dom';
+import Equipment from './Equipment';
 
-const Setting = (props: RouteComponentProps) => {
+const Setting = (props: RouteComponentProps<{}>) => {
+
 
     const returnBack = () => {
         props.history.push('/');
@@ -12,17 +14,19 @@ const Setting = (props: RouteComponentProps) => {
             <div className = "settingWrapBox">
                 <div className = "settingCheckOptionBox">
                     <ul className = "settingCheckOptionCollection">
-                        <li className = "settingCheckOption">마부</li>
+                        <li className = "settingCheckOption"><Link to ='/setting/equipment'>마부</Link></li>
                         <li className = "settingCheckOption">딜표</li>
                         <li className = "settingCheckOption">버프력</li>
                     </ul>
                 </div>
+            </div>
+            <Route path = '/setting' exact render = {() => 
                 <div className = "settingContentBox">
                     <div className = 'returnBackButtonBox'>
                         <button className = 'returnBackButton' onClick = {returnBack}>설정하기</button>
                     </div>
-                </div>
-            </div>
+                </div>} />
+            <Route path = '/setting/:option' component = {Equipment} />
         </>
     )
 }
