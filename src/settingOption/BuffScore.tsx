1d9b1685d157a9bb11b3b7032d9c37839099d28e
goldmembrane <byenghyunhwang@Byengui-MacBook-Pro.local>
2021-07-07 20:47:31 +0900

    make up buffscore.tsx file and re-positioning return back button

--- src/settingOption/BuffScore.tsx
+++ src/settingOption/BuffScore.tsx
@@ -1,9 +1,29 @@
 import * as React from 'react'
 
 const BuffScore = () => {
+    
     return (
         <>
-            <div>버프력 표시</div>
+            <div className = "buffScoreWrapBox">
+                <div className = "buffScoreTitleBox">
+                    <h3>버프력 컷 설정</h3>
+                </div>
+                <div className = "buffScoreContentBox">
+                    <div className = "seraphimBufferBox">
+                        <div className = "seraphimLabel">세라핌 : </div>
+                        <input className = "seraphimBuffCut" type = "text" />
+                    </div>
+                    <div className = "saintBufferBox">
+                        <div className = "saintLabel">세인트 : </div>
+                        <input className = "saintBuffCut" type = "text" />
+                    </div>
+                    <div className = "hecateBox">
+                        <div className = "hecateLabel">헤카테 : </div>
+                        <input className = "hecateBuffCut" type = "text" />
+                    </div>
+                </div>
+
+            </div>
         </>
     )
 }
