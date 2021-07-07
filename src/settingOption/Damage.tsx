64b32a4a3961a8b4d17d627bf28d5cbecb60724d
goldmembrane <byenghyunhwang@Byengui-MacBook-Pro.local>
2021-07-07 20:15:43 +0900

    create settingOption folder and inner files

---
+++ src/settingOption/Damage.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+
+const Damage = () => {
+    return (
+        <>
+        </>
+    )
+}
+
+export default Damage;
---
+++ src/settingOption/Equipment.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+
+const Equipment = () => {
+    return (
+        <>
+        </>
+    )
+}
+
+export default Equipment;
