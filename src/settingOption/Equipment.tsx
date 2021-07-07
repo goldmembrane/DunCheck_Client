0e657c0f229f656395da0dbbf8c4ccbc7ba630f2
goldmembrane <byenghyunhwang@Byengui-MacBook-Pro.local>
2021-07-07 21:07:35 +0900

    make up equipment.tsx file and ready for sub-routing in equipment

--- src/settingOption/Equipment.tsx
+++ src/settingOption/Equipment.tsx
@@ -3,7 +3,19 @@ import * as React from 'react';
 const Equipment = () => {
     return (
         <>
-            <div>장비 표시</div>
+            <div className = "equipmentWrapBox">
+                <div className = "equipmentTitleBox">
+                    <h3>장비 컷 설정</h3>
+                </div>
+                <nav>
+                    <ul>
+                        <li>장비</li>
+                        <li>아바타</li>
+                        <li>크리쳐</li>
+                        <li>탈리스만</li>
+                    </ul>
+                </nav>
+            </div>
         </>
     )
 }
