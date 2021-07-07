9b4b62e239a4b3da643a376dc3ac181d0efa822d
goldmembrane <byenghyunhwang@Byengui-MacBook-Pro.local>
2021-07-08 08:54:51 +0900

    make up Avatar.tsx file

--- src/settingOption/equipment/Avatar.tsx
+++ src/settingOption/equipment/Avatar.tsx
@@ -3,7 +3,21 @@ import * as React from 'react';
 const Avatar = () => {
     return (
         <>
-            <div>아바타 표시</div>
+            <div className = "avatarWrapBox">
+                <div className = "avatarTitleBox">
+                    <h4>아바타 컷 설정</h4>
+                </div>
+                <div className = "avatarContentBox">
+                    <div className = "checkDealPlatinumBox">
+                        <div className = "dealPlatinumLabel">딜 플티 여부 : </div>
+                        <input className = "checkDealPlatinum" type = "checkbox" />
+                    </div>
+                    <div className = "checkResistDarkBox">
+                        <div className = "resistDarkLabel">암속성 저항 수치 만족 여부 : </div>
+                        <input className = "checkResistDark" type = "checkbox" />
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
