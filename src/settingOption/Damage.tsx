ad6d7c2217be98396679b6e9a7ca33cd4448b46a
goldmembrane <byenghyunhwang@Byengui-MacBook-Pro.local>
2021-07-07 21:00:25 +0900

    make up damage.tsx file

--- src/settingOption/Damage.tsx
+++ src/settingOption/Damage.tsx
@@ -3,7 +3,21 @@ import * as React from 'react';
 const Damage = () => {
     return (
         <>
-            <div>딜표 표시</div>
+            <div className = "damageWrapBox">
+                <div className = "damageTitleBox">
+                    <h3>딜표 컷 설정</h3>
+                </div>
+                <div className = "damageContentBox">
+                    <div className = "pureDealerBox">
+                        <div className = "pureDealerLabel">퓨어 딜러 : </div>
+                        <input className = "pureDealerCut" type = "text" />
+                    </div>
+                    <div className = "synergyDealerBox">
+                        <div className = "synergyDealerLabel">시너지 딜러 : </div>
+                        <input className = "synergyDealerCut" type = "text" />
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
