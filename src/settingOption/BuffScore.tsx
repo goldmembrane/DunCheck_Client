47821eb5ff273ce5a200c82bee3c9ac2456b0529
goldmembrane <byenghyunhwang@Byengui-MacBook-Pro.local>
2021-07-10 00:53:39 +0900

    create buff state and save input buff number

--- src/settingOption/BuffScore.tsx
+++ src/settingOption/BuffScore.tsx
@@ -1,6 +1,29 @@
 import * as React from 'react'
+import { useState } from 'react';
 
-const BuffScore = () => {
+type buffProps = {
+    buffSubmit: (buff: {seraphim: number, saint: number, hecate: number}) => void
+}
+
+const BuffScore = ({buffSubmit}: buffProps) => {
+
+    const [seraphimBuff, setSeraphimBuff] = useState(0);
+
+    const [saintBuff, setSaintBuff] = useState(0);
+
+    const [hecateBuff, setHecateBuff] = useState(0);
+
+    const onSeraphimChange = (e:React.ChangeEvent<HTMLInputElement>) => {
+        setSeraphimBuff((e.target as HTMLInputElement).valueAsNumber);
+    }
+
+    const onSaintChange = (e:React.ChangeEvent<HTMLInputElement>) => {
+        setSaintBuff((e.target as HTMLInputElement).valueAsNumber);
+    }
+
+    const onHecateChange = (e:React.ChangeEvent<HTMLInputElement>) => {
+        setHecateBuff((e.target as HTMLInputElement).valueAsNumber);
+    }
     
     return (
         <>
@@ -11,15 +34,15 @@ const BuffScore = () => {
                 <div className = "buffScoreContentBox">
                     <div className = "seraphimBufferBox">
                         <div className = "seraphimLabel">세라핌 : </div>
-                        <input className = "seraphimBuffCut" type = "text" />
+                        <input className = "seraphimBuffCut" type = "number" value = {seraphimBuff} onChange = {onSeraphimChange}/>
                     </div>
                     <div className = "saintBufferBox">
                         <div className = "saintLabel">세인트 : </div>
-                        <input className = "saintBuffCut" type = "text" />
+                        <input className = "saintBuffCut" type = "number" value = {saintBuff} onChange = {onSaintChange}/>
                     </div>
                     <div className = "hecateBox">
                         <div className = "hecateLabel">헤카테 : </div>
-                        <input className = "hecateBuffCut" type = "text" />
+                        <input className = "hecateBuffCut" type = "number" value = {hecateBuff} onChange = {onHecateChange}/>
                     </div>
                 </div>
 
